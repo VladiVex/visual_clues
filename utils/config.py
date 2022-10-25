30c1168e7c67a5b78e9483e8095bd9c48c28ecd1
Ilan Aizelman <iWX1127365@china.huawei.com>
2022-10-25 12:24:39 +0300

    added more scripts to make it work

--- utils/config.py
+++ utils/config.py
@@ -4,5 +4,7 @@ config = {
     'blip_model_url_large': 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_large_retrieval_coco.pth',
     'blip_vit_large': 'large',
     'blip_image_size': 384,
-    'clip_checkpoints': "openai/clip-vit-base-patch32"
+    'clip_checkpoints': "openai/clip-vit-base-patch32",
+    'caption_model_ckpt_ofa': 'ckpt/finetuned/caption_huge_best.pt',
+    'ofa_bpe_path': 'utils/BPE'
 }
